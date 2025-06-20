bb6b4db7a9d9dd7bea032b3f541a9335d6b43fe8
vicferx <vicferx@gmail.com>
2025-06-20 18:08:18 -0400

    Etapa 2 reproductor no funcionado

--- ETAPA1/videofollower.cpp
+++ ETAPA1/videofollower.cpp
@@ -11,14 +11,31 @@ VideoFollower::VideoFollower(std::string name, std::string topicName, QWidget *p
     //buton->setFixedSize(10, 10, 200, 50);
     boton->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
 
+    // Crear el reproductor de video
+    mediaPlayer = new QMediaPlayer(this);
+    videoWidget = new QVideoWidget(this);
+
     // Crear y configurar el layout vertical
     QVBoxLayout* layout = new QVBoxLayout(this);
     layout->addWidget(boton);
+    layout->addWidget(videoWidget);
     setLayout(layout);
+
+    // Conectar el botón para reproducir el video al hacer clic
+    connect(boton, &QPushButton::clicked, this, &VideoFollower::reproducirVideo);
 }
     // Sobrescribe el método update para cambiar el texto del botón
 void VideoFollower::update(const QString& message)
 {
-boton->setText(message);
+    boton->setText(message);
+    ultimoUrl = message;
+}
+
+void VideoFollower::reproducirVideo()
+{
+    if (!ultimoUrl.isEmpty()) {
+        mediaPlayer->setSource(QUrl::fromUserInput(ultimoUrl));
+        mediaPlayer->play();
+    }
 }
 
